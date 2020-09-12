706fc2303f63677a41f0f52363f9fbfbfe3f87e7
Hossein HassanNejad <hosseinspell@gmail.com>
2020-09-12 09:44:16 +0430

    rtl navigation drawer

--- app/src/main/java/com/husen/android/bitgram/BitGramActivity.kt
+++ app/src/main/java/com/husen/android/bitgram/BitGramActivity.kt
@@ -2,11 +2,39 @@ package com.husen.android.bitgram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class BitGramActivity : AppCompatActivity() {
+class BitGramActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bitgram)
+        setContentView(R.layout.navigation_drawer)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navigationView: NavigationView = findViewById(R.id.navigation_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.open_drawer,
+            R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // RTL navigation drawer
+        toolbar.setNavigationOnClickListener {
+            if (drawerLayout.isDrawerOpen(Gravity.RIGHT)) {
+                drawerLayout.closeDrawer(Gravity.RIGHT)
+            } else {
+                drawerLayout.openDrawer(Gravity.RIGHT)
+            }
+        }
 
         val isFragmentContainerEmpty = savedInstanceState == null
         if (isFragmentContainerEmpty) {
@@ -15,5 +43,11 @@ class BitGramActivity : AppCompatActivity() {
                     .add(R.id.fragmentContainer, BitGramFragment.newInstance())
                     .commit()
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var id = item.itemId
+
+        return true
     }
 }
