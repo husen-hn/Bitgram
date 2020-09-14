b83a95840253872421d4ca9baa3801c6274a1385
Hossein HassanNejad <hosseinspell@gmail.com>
2020-09-14 13:06:20 +0430

    searchview

--- app/src/main/java/com/husen/android/bitgram/BitGramActivity.kt
+++ app/src/main/java/com/husen/android/bitgram/BitGramActivity.kt
@@ -2,12 +2,20 @@ package com.husen.android.bitgram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
+import android.view.Menu
 import android.view.MenuItem
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
+import com.husen.android.bitgram.NavigationDrawerItems.SuggestionFragment
+import kotlinx.android.synthetic.main.navigation_drawer.*
 
 class BitGramActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +26,10 @@ class BitGramActivity : AppCompatActivity(), NavigationView.OnNavigationItemSele
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navigationView: NavigationView = findViewById(R.id.navigation_view)
 
+        setSupportActionBar(toolbar)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        //Navigation Drawer
         val toggle = ActionBarDrawerToggle(
             this,
             drawerLayout,
@@ -36,18 +48,58 @@ class BitGramActivity : AppCompatActivity(), NavigationView.OnNavigationItemSele
             }
         }
 
-        val isFragmentContainerEmpty = savedInstanceState == null
-        if (isFragmentContainerEmpty) {
-            supportFragmentManager
-                    .beginTransaction()
-                    .add(R.id.fragmentContainer, BitGramFragment.newInstance())
-                    .commit()
+        //Display first fragment
+        val currentFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+
+        if (currentFragment == null) {
+            val fragment = BitGramFragment.newInstance()
+            replaceFragment(fragment)
         }
+//        val isFragmentContainerEmpty = savedInstanceState == null
+//        if (isFragmentContainerEmpty) {
+//            supportFragmentManager
+//                    .beginTransaction()
+//                    .add(R.id.fragmentContainer, BitGramFragment.newInstance())
+//                    .commit()
+//        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        var id = item.itemId
+        when(item.itemId) {
+            R.id.familiarity_id -> { Log.d("XXXX", "familiarity_id")}
+            R.id.suggestion_id -> {
+                val fragment = SuggestionFragment.newInstance()
+                replaceFragment(fragment)
+            }
+            R.id.privacy_id -> {Log.d("XXXX", "privacy_id")}
+            R.id.about_id -> {Log.d("XXXX", "about_id")}
+            R.id.star_id -> {Log.d("XXXX", "star_id")}
+            R.id.share_id -> {Log.d("XXXX", "share_id")}
+            R.id.changes_id -> {Log.d("XXXX", "changes_id")}
+        }
+        drawer_layout.closeDrawer(GravityCompat.END)
 
         return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.search_ic -> Toast.makeText(applicationContext, "search", Toast.LENGTH_SHORT).show()
+        }
+        return false
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
--- app/src/main/java/com/husen/android/bitgram/BitGramFragment.kt
+++ app/src/main/java/com/husen/android/bitgram/BitGramFragment.kt
@@ -2,23 +2,21 @@ package com.husen.android.bitgram
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
 import kotlinx.android.synthetic.main.fragment_bitgram.*
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
 import java.math.RoundingMode
@@ -119,12 +117,15 @@ class BitGramFragment : Fragment() {
                     gramItem.changePrice,
                     gramItem.lastPrice,
                     usaPercent,
-                    usaPercentIcon))}%"
+                    usaPercentIcon
+                ))}%"
             }
 
         }
-        private fun calPercent(changePrice: String, lastPrice: String,
-                               tvPercent: TextView, ivPercent: ImageView): String {
+        private fun calPercent(
+            changePrice: String, lastPrice: String,
+            tvPercent: TextView, ivPercent: ImageView
+        ): String {
 
             val initialNum = lastPrice.toDouble() - changePrice.toDouble()
 
@@ -138,8 +139,10 @@ class BitGramFragment : Fragment() {
 
             return RoundPercent
         }
-        private fun changePercentColorAndIcon(percent: Double, tvPercent: TextView,
-                                              ivPercent: ImageView) {
+        private fun changePercentColorAndIcon(
+            percent: Double, tvPercent: TextView,
+            ivPercent: ImageView
+        ) {
             if (percent >= 0) {
                 tvPercent.setTextColor(resources.getColor(R.color.Green))
                 ivPercent.load(R.drawable.up)
@@ -150,12 +153,13 @@ class BitGramFragment : Fragment() {
         }
     }
 
-    private inner class BitAdapter(private val gramItems: List<GramItem>,
-                                   var dataSourceList: HashMap<String, DataSourceItem>)
+    private inner class BitAdapter(
+        private val gramItems: List<GramItem>,
+        var dataSourceList: HashMap<String, DataSourceItem>
+    )
         : RecyclerView.Adapter<BitHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BitHolder {
-            val view
-                    = layoutInflater.inflate(R.layout.bit_list_card_view, parent, false)
+            val view = layoutInflater.inflate(R.layout.bit_list_card_view, parent, false)
             return BitHolder(view)
         }
 
