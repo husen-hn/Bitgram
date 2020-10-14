addb328878b8ecfc565339161325627a64230543
Hossein HassanNejad <hosseinspell@gmail.com>
2020-10-14 12:59:19 +0330

    databinding in detail fragments

--- app/src/main/java/com/husen/android/bitgram/BitGramActivity.kt
+++ app/src/main/java/com/husen/android/bitgram/BitGramActivity.kt
@@ -2,13 +2,17 @@ package com.husen.android.bitgram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.husen.android.bitgram.databinding.ActivityBitgramBinding
 
 private const val TAG = "BitGramActivity"
 
 class BitGramActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityBitgramBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bitgram)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_bitgram)
     }
 }
